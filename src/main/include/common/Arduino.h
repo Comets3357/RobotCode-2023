4143c88ac5a7ef19f5581ec74a6d936a760e7213
Hendrik <hnsorens@gmail.com>
2023-01-09 14:10:39 -0500

    Added a base arduino code that will be added onto once we have specific functions for the arduino

--- src/main/include/common/Arduino.h
+++ src/main/include/common/Arduino.h
@@ -1,6 +1,27 @@
 #pragma once
 
+#include <frc/SerialPort.h>
+#include <time.h>
+
+#include "RobotData.h"
+
+struct ArduinoData
+{
+
+};
+
 class Arduino
 {
+public:
+    void RobotPeriodic(const RobotData &robotData, ArduinoData arduinoData);
+    void DisabledPeriodic();
+    void RobotInit();
+
+    int failedTransfers = 0;
+private:
+
+    bool arduinoWorking = true;
+
+    frc::SerialPort *arduinoPort;
 
 };
