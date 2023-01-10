f7d980093151bafbe5e5fac0c7a6843b722c6747
Benjamin Chatman <bennyjjumpin@gmail.com>
2023-01-10 18:27:05 -0500

    Made elevator branch and uncommented code.

--- src/main/cpp/subsystems/Elevator.cpp
+++ src/main/cpp/subsystems/Elevator.cpp
@@ -1,33 +1,33 @@
-// #include <subsystems/Elevator.h>
+#include <subsystems/Elevator.h>
 
 
 
 
-// void Elevator::RobotInit(const RobotData &robotData, ElevatorData &elevatorData)
-// {
-//     elevatorMotor.RestoreFactoryDefaults();
-//     elevatorMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
-//     elevatorMotor.SetInverted(false);
-//     elevatorPIDController.SetP(0.1, 0);
-//     elevatorMotor.BurnFlash();
-// }
+void Elevator::RobotInit(const RobotData &robotData, ElevatorData &elevatorData)
+{
+    elevatorMotor.RestoreFactoryDefaults();
+    elevatorMotor.SetIdleMode(rev::CANSparkMax::IdleMode::kBrake);
+    elevatorMotor.SetInverted(false);
+    elevatorPIDController.SetP(0.1, 0);
+    elevatorMotor.BurnFlash();
+}
 
-// void Elevator::RobotPeriodic(const RobotData &robotData, ElevatorData &elevatorData)
-// {
+void Elevator::RobotPeriodic(const RobotData &robotData, ElevatorData &elevatorData)
+{
     
-// }
+}
 
-// bool Elevator::RunElevatorToPos(double pos, int PIDSlot, bool &elevatorRunning)
-// {
-//     elevatorPIDController.SetReference(pos, rev::ControlType::kPosition, PIDSlot);
-//     if (elevatorEncoder.GetPosition() > pos - positionError && elevatorEncoder.GetPosition() < pos + positionError)
-//     {
-//         elevatorRunning = true;
-//         return true;
-//     }
-//     else
-//     {
-//         elevatorRunning = false;
-//         return false;
-//     }
-// }
+bool Elevator::RunElevatorToPos(double pos, int PIDSlot, bool &elevatorRunning)
+{
+    elevatorPIDController.SetReference(pos, rev::ControlType::kPosition, PIDSlot);
+    if (elevatorEncoder.GetPosition() > pos - positionError && elevatorEncoder.GetPosition() < pos + positionError)
+    {
+        elevatorRunning = true;
+        return true;
+    }
+    else
+    {
+        elevatorRunning = false;
+        return false;
+    }
+}
--- src/main/include/subsystems/Elevator.h
+++ src/main/include/subsystems/Elevator.h
@@ -5,24 +5,23 @@
 
 class Elevator
 {
-
 public:
 
-//     struct ElevatorData
-//     {
-//         bool elevatorRunning = false;
-//     };
+    struct ElevatorData
+    {
+        bool elevatorRunning = false;
+    };
 
-//     void RobotInit(const RobotData &robotData, ElevatorData &elevatorData);
-//     void RobotPeriodic(const RobotData &robotData, ElevatorData &elevatorData);
+    void RobotInit(const RobotData &robotData, ElevatorData &elevatorData);
+    void RobotPeriodic(const RobotData &robotData, ElevatorData &elevatorData);
 
-// private:
+private:
 
-//     bool RunElevatorToPos(double pos, int PIDSlot, bool &elevatorRunning);
+    bool RunElevatorToPos(double pos, int PIDSlot, bool &elevatorRunning);
 
-//     double positionError = 1;
+    double positionError = 1;
 
-//     rev::CANSparkMax elevatorMotor{100, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
-//     rev::SparkMaxRelativeEncoder elevatorEncoder = elevatorMotor.GetEncoder();
-//     rev::SparkMaxPIDController elevatorPIDController = elevatorMotor.GetPIDController();
+    rev::CANSparkMax elevatorMotor{100, rev::CANSparkMaxLowLevel::MotorType::kBrushless};
+    rev::SparkMaxRelativeEncoder elevatorEncoder = elevatorMotor.GetEncoder();
+    rev::SparkMaxPIDController elevatorPIDController = elevatorMotor.GetPIDController();
 };
