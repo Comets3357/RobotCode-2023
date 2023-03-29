139908bd867f3b8703a3c7afc915548b32f8a37f
ishan <forishansinha@outlook.com>
2023-03-28 22:58:22 -0400

    Odometry resetting code ready to be tested using two different limelights

--- src/main/include/common/Limelight.h
+++ src/main/include/common/Limelight.h
@@ -43,7 +43,8 @@ private:
     double GetDistance(double targetHeight, double vertAngleOffset);
 
     // std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight"); 
-    LimelightHelpers::LimelightResultsClass llresults; //= LimelightHelpers::getLatestResults();
+    LimelightHelpers::LimelightResultsClass llresultsOne; //= LimelightHelpers::getLatestResults();
+    LimelightHelpers::LimelightResultsClass llresultsTwo; 
     std::vector<double> limelightOdometry = {0,0,0,0,0,0};
 
 
@@ -55,37 +56,13 @@ private:
 
     int numberOfTagsInView = 0;
 
-    double distanceFromTarget = 0;
-    double angleOff = 0;
-
-    double aprilTagHeight = 13.0;
-    double lowerPollHeight = 22.125; // need to tune
-    double higherPollHeight = 41.875; // need to tune
-
-    double limelightHeight = 11.03;
-
-    double cameraDistanceFromCenter = 11.84;
-    double midEndEffectorDistanceFromCenter = 13 + 12; // need to tune
-    double highEndEffectorDistanceFromCenter = 13 + 38; // need to tune
-    
-    double limelightAngle = 25;
-
     // 63.3 x 49.7
-    double upperAngleOffset = 12.66;
-    double lowerAngleOffset = 0;
-
-    double inchesToMeters = 0.0254;
-
-    double distanceFromCenterOfRobot = 0;
-    double angleFromCenterOfRobot = 0;
-
-    double secondAngleFromCenter = 0;;
-    double finalAngle;
 
     units::radian_t gyroRadians{0};
     frc::Rotation2d gyroRotation{gyroRadians};
 
-    double distanceBetweenMidAndTopPoll = 17;
+    int limelightOneID = 0;
+    int limelightTwoID = 0;
 
 
 
